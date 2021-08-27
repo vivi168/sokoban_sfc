6ee83609a724fc565f0d63b0257ee0f3a7772ab4
Vivien B <vivienbihl@gmail.com>
2021-08-28 00:02:04 +0200

    Define file for spc700

--- music/main.asm
+++ music/main.asm
@@ -1,12 +1,13 @@
 .spc700
 
+.include registers.inc
 .include macros.inc
 
 .org 0600
 .base 0000
 
-.call SET_FIRN 20, 6
-.call SET_VNENVX 20, 4
+.call SET_DSP_REG FIR6, 20
+.call SET_DSP_REG V4ENVX, 20
 
 wait:
     bra @wait
